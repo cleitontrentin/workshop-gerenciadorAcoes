6857acb92600c506428f8a547021cefd11355634
Cleiton Trentin <cleiton.trentin@gmail.com>
2024-02-08 18:46:44 -0300

    Criando classe de usuário

---
+++ src/main/java/com/cleitontrentin/gerenciadorAcoes/resource/UserResource.java
@@ -0,0 +1,22 @@
+package com.cleitontrentin.gerenciadorAcoes.resource;
+
+import org.apache.catalina.connector.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cleitontrentin.gerenciadorAcoes.Entites.User;
+
+@RestController
+@RequestMapping(value = "/Users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "cleiton", "cleiton.trentin@gmail.com", "999566024", "123456");
+		return ResponseEntity.ok().body(u);
+	
+	}
+
+}
