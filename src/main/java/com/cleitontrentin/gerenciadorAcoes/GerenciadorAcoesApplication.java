dbc5d1173d9d8a2d88ddea6f201b6b78979f6d29
cleitontrentin <cleiton.trentin@gmail.com>
2024-02-08 17:02:02 -0300

    Project created

---
+++ src/main/java/com/cleitontrentin/gerenciadorAcoes/GerenciadorAcoesApplication.java
@@ -0,0 +1,13 @@
+package com.cleitontrentin.gerenciadorAcoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorAcoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorAcoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cleitontrentin/gerenciadorAcoes/GerenciadorAcoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleitontrentin.gerenciadorAcoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorAcoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
